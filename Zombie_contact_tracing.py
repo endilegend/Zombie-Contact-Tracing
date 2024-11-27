05108126168159b4eb6365785e69311a95768783
Endi <troqeendi@gmail.com>
2024-11-27 10:11:51 -0500

    finished part 1-7

--- Zombie_contact_tracing.py
+++ Zombie_contact_tracing.py
@@ -1,4 +1,3 @@
-
 zeros = []
 potential = []
 neither = []
@@ -8,41 +7,27 @@ virality = {}
 spreaders = []
 contacted = []
 
+def maximumDistance(potential, person, people, visited):
+    if person in potential:
+        return 0
 
-distances = {}
-distanceOne = []
+    dist = []
+    for contact in people[person]:
+        dist.append(1 + maximumDistance(potential, contact, people, visited))
 
-
-def maximumDistance(everyone, zeros, distanceOne):
-    if len(everyone) == 0:
-        return ""
-
-    z = 99
-    
-    x = everyone.pop()
-    if x in zeros:
-        z = 0
-    elif x in distanceOne:
-        z = 1
-
-    return f"{x}: {z}\n" + maximumDistance(everyone, potential, zeros)
+    return max(dist)
 
 
 def patientZero(spreaders):
     for i in spreaders:
         if i not in contacted:
             zeros.append(i)
-
     return  ", ".join(zeros)
 
 def potentialZombies(contacted):
     for i in contacted:
         if i not in spreaders and i not in potential:
             potential.append(i)
-            for key, value in distances.items():
-                if i in value:
-                    distanceOne.append(key)
-        
     return ", ".join(potential)
 
 def neitherZZ(total):
@@ -61,21 +46,17 @@ def highestContacted(mostContacted):
     mostContacted = [key for key, value in mostContacted.items() if value == highest]
     return ", ".join(mostContacted)
 
+file = "zombie-input/Dataset5.txt"
 
-    
-
-
-file = "Dataset1.txt"
-
-
-
+people = {}
 mostContacted = {}
-with open(file, "r") as r, open("outputy.txt", "a") as f:
+with open(file, "r") as r, open("outputy.txt", "w") as f:
     f.write("Contact records:\n")
     for line in r:
         viral = []
 
         line = line.strip()
+        line = line.strip("\n")
         names = line.split(",")
 
         spreaders.append(names[0])
@@ -86,21 +67,16 @@ with open(file, "r") as r, open("outputy.txt", "a") as f:
         contacts = ", ".join(names[1:])
         f.write(f"  {names[0]} had contact with {contacts}\n")
 
-        distances[names[0]] = names[1:]
-
+        people[names[0]] = names[1:]
 
         virality[names[0]] = len(viral)
 
         for i in names[1:]:
-            mostContacted[i] = 1 + mostContacted.get(i, 0)  
+            mostContacted[i] = 1 + mostContacted.get(i, 0)
         for i in names:
             everyone.add(i)
-      
 
     f.write("\n")
-
-
-
 
     total = set(contacted + zeros)
 
@@ -111,7 +87,14 @@ with open(file, "r") as r, open("outputy.txt", "a") as f:
     f.write(f"Most contacted people: {highestContacted(mostContacted)}\n")
     f.write("\n")
     f.write("Maximum distance from a potential zombie:\n")
-    f.write(maximumDistance(everyone, zeros, distanceOne))
+    everyone = list(everyone)
+    distances = {}
+    for person in everyone:
+        distances[person] = maximumDistance(potential, person, people, set())
 
+    distances = dict(sorted(distances.items(), key = lambda item: item[1], reverse = True))
+    
+    for key, value in distances.items():
+        f.write(f"{key}: {value}\n")
 
-
+print(distances)
